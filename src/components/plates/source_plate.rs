56484ec870057f8a08d804318b167a905c7b4254
Emilia <contact@emiliaallison.com>
2023-06-13 12:51:16 -0400

    Fix miniscule column/row headers

--- src/components/plates/source_plate.rs
+++ src/components/plates/source_plate.rs
@@ -99,7 +99,9 @@ pub fn SourcePlate(props: &SourcePlateProps) -> Html {
     let column_header = {
         let headers = (1..=props.source_plate.plate.size().1)
             .map(|j| {
-                html! {<th>{format!("{:0>2}", j)}</th>}
+                html! {<th>
+                    {format!("{:0>2}", j)}
+                    </th>}
             })
             .collect::<Html>();
         html! {<tr><th />{ headers }</tr>}
